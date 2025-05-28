aaeb67c634a44bf1b25d982d4e39ca5524129ca2
SmithiM <112447224+smithimah09@users.noreply.github.com>
2025-05-27 23:11:53 -0400

    Update move_zeroes.java

--- Easy/arrays/move-zeroes/move_zeroes.java
+++ Easy/arrays/move-zeroes/move_zeroes.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int snowball =0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 0){
+                snowball++;
+            } else if (snowball>0){
+                nums[i-snowball] = nums[i];
+                nums[i] = 0;
+            }
+        }
+        }
+    }
