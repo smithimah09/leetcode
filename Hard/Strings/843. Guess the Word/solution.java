c89c8a2e64e182ff608b44e1aae55c4d99342e11
SmithiM <112447224+smithimah09@users.noreply.github.com>
2025-06-05 14:07:46 -0400

    Create solution.java

---
+++ Hard/Strings/843. Guess the Word/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void findSecretWord(String[] wordlist, Master master) {
+        Random random = new Random();
+        for(int i = 0, matches = 0; i < 10 && matches != 6; i ++){
+            String guess = wordlist[random.nextInt(wordlist.length)];
+            matches = master.guess(guess);
+            List<String> candidates = new ArrayList<>();
+            for(String word: wordlist){
+                if(matches == getMatches(guess, word)){
+                    candidates.add(word);
+                }
+            }
+            
+            wordlist = candidates.toArray(new String[0]);
+        }
+    }
+    
+    private int getMatches(String word1, String word2){
+        int matches = 0;
+        for(int i = 0; i < word1.length(); i ++){
+            if(word1.charAt(i) == word2.charAt(i)){
+                matches ++;
+            }
+        }
+        
+        return matches;
+    }
+}
